9526691293193f365099977ead4e21e633fdd15a
Gokul Priyadharsan <gokuldharsan95@gmail.com>
2024-11-01 15:53:56 +0530

    Revert "Task Management Application Code Commit"
    This reverts commit 7e986b48341388c71e1e21ff3032160f814c487f.

--- TMA.Web/ClientApp/src/app/modal/TaskDto.ts
+++
@@ -1,8 +0,0 @@
-export class TaskDto {
-  id: number | null = null;
-  title: string | null = null;
-  description: string | null = null;
-  isCompleted: boolean = false;
-  createdAt: Date | null=null;
-  updatedAt: Date | null = null;
-}
